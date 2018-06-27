17cfacfefd72e5531ef4438778e8f8ab2a03fdce
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2018-06-27 19:28:45 -0300

    fix

--- js/contato.js
+++ js/contato.js
@@ -2,6 +2,6 @@ $('#gformContact').on('submit', function (e) {
     $('#gformContact *').fadeOut(1000);
     $('#gformContact').prepend('' +
         '<div class="clear">' +
-        '<div class="response text-success text-center alert bg-darkest-gray" id="mce-success-response"><span class="label well-lg">Obrigado pelo seu contato!<br>O interesse pelo curso foi cadastrado com sucesso, retornaremos o mais rápido possível.</span></div>' +
+        '<div class="response text-success text-center alert bg-darkest-gray" id="mce-success-response"><span class="text-white well-lg">Obrigado pelo seu contato!<br>O interesse pelo curso foi cadastrado com sucesso, retornaremos o mais rápido possível.</span></div>' +
         '</div>');
 });
